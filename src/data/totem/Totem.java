51ac8e2b155097e3798d5e81df6206742f6d6611
igorcardosoy <igorfilipicardoso@gmail.com>
2024-08-07 21:20:53 -0300

    refactor: removing name atribute from Totem

--- src/data/totem/Totem.java
+++ src/data/totem/Totem.java
@@ -10,19 +10,18 @@ import java.util.List;
 
 public class Totem implements FlightDataObserver {
 
-    private final String name;
     private final ITotemView view;
     private final List<FlightData> flights;
     private final List<String> flightsState;
 
     public Totem(String name, ITotemView view, State ...flights) {
-        this.name = name;
         this.view = view;
+        view.setTotemTitle(name);
+
         this.flights = new ArrayList<>(5);
         this.flightsState = new ArrayList<>(4);
 
         Arrays.stream(flights).toList().forEach(flight -> this.flightsState.add(flight.getClass().getSimpleName()));
-        view.setTotemTitle(getName());
     }
 
     @Override
@@ -37,9 +36,5 @@ public class Totem implements FlightDataObserver {
 
     private boolean isInterested(FlightData flight){
         return flightsState.contains(flight.getState().getClass().getSimpleName());
-    }
-
-    public String getName() {
-        return name;
     }
 }
